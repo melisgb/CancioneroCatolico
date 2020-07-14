32d40ff0b2e6b945a548ac5417d67d7ef73d12c7
melisgb <gloriamelissa1@hotmail.com>
2020-07-14 19:56:47 +0100

    Chip Group implemented
    Not yet functionally filtering, for now only shows the data filters

--- app/src/main/java/com/example/cancionerocatolico/ViewSongsByKeyActivity.kt
+++ app/src/main/java/com/example/cancionerocatolico/ViewSongsByKeyActivity.kt
@@ -2,15 +2,30 @@ package com.example.cancionerocatolico
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.HorizontalScrollView
 
 class ViewSongsByKeyActivity : AppCompatActivity() {
+    var selectedFilters : String = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_songs_by_key)
-
+        
         val intentExtra = intent.extras
         val key = intentExtra!!.getString("key")
 
+        if(key == "parts"){
+            val horizontalViewSeasons = findViewById<HorizontalScrollView>(R.id.chipsForSeasons)
+            horizontalViewSeasons.visibility = View.INVISIBLE
+        }
+        if(key == "seasons"){
+            val horizontalViewParts = findViewById<HorizontalScrollView>(R.id.chipsForParts)
+            horizontalViewParts.visibility = View.INVISIBLE
+        }
+
+    }
+
+    fun chipClicked(view: View) {
 
     }
 }
